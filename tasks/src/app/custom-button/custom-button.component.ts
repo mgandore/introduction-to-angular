3cc16a782561d6e9a667da81f9f08465d7a3169d
mgandore <mihai15.catalin@yahoo.com>
2022-07-20 12:34:26 +0300

    Make requested changes.
    Added the word 'public' to the public methods to improve code's
    readability.
    Deleted unnecessary :html element in custom-button component and
    title of AppComponent class.

--- tasks/src/app/custom-button/custom-button.component.ts
+++ tasks/src/app/custom-button/custom-button.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomButtonComponent implements OnInit {
 
-	constructor() { }
+	public constructor() { }
 
-	ngOnInit(): void {
+	public ngOnInit(): void {
 	}
 
 }
