ac8269ae4d932d45203cbae90058c02980d9dcb9
mgandore <mihai15.catalin@yahoo.com>
2022-07-20 13:50:49 +0300

    Add second task, change angular.json
    Disabled cache

--- tasks/src/app/custom-button/custom-button.component.ts
+++ tasks/src/app/custom-button/custom-button.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomButtonComponent implements OnInit {
 
+	public numOfClicks = 0;
+
 	public constructor() { }
+
+	public increment() {
+		this.numOfClicks++;
+	}
 
 	public ngOnInit(): void {
 	}
